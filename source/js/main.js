0502289ea7e491ad34718abb3a9c36d47fd2e96b
dmm <ikzsl@yandex.ru>
2020-04-08 08:28:21 +0300

    Добавляет слайдер тренеры

--- source/js/main.js
+++ source/js/main.js
@@ -1,122 +1,56 @@
 'use strict';
 
-
 var imask = window.IMask;
-
-imask(document.getElementById('modal-tel'), {
-  mask: '+{7}(000)000-00-00',
-});
 
 imask(document.getElementById('form-tel'), {
   mask: '+{7}(000)000-00-00',
 });
 
 
-var modalWindow = document.querySelector('.modal-feedback');
-var modalCloseButton = document.querySelector('.modal-feedback__close');
-var callbackButton = document.querySelector('.button--callback');
-var popupLayer = document.querySelector('.popup-layer');
+// -------swiper
+var Swiper = window.Swiper;
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  centeredSlidesBounds: true,
+  // centeredSlides: true,
+  // spaceBetween: 30,
 
+  // Optional parameters
+  direction: 'horizontal',
 
-var showPopup = function () {
-  modalWindow.classList.remove('modal-feedback--off');
-  popupLayer.classList.remove('popup-layer--off');
-  document.body.classList.add('lock');
+  breakpoints: {
+    // when window width is >= 320px
 
-};
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 2,
+      slidesPerGroup: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      slidesPerGroup: 4,
+    },
 
-var hidePopup = function () {
+  },
+  // slidesOffsetBefore: 100,
+  // slidesOffsetAfter: 100,
 
-  modalWindow.classList.add('modal-feedback--off');
-  popupLayer.classList.add('popup-layer--off');
-  document.body.classList.remove('lock');
+  // loop: true,
+  // centeredSlides: true,
 
-};
+  // If we need pagination
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
 
-var onPopupLayerClick = function () {
-  hidePopup();
-  popupLayer.removeEventListener('click', onPopupLayerClick);
-};
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-var onEscKeyDown = function (evt) {
-  if (evt.keyCode === 27) {
-    onPopupLayerClick();
-  }
-};
+});
 
-var onCallbackButtonClick = function () {
-  showPopup();
-  document.addEventListener('keydown', onEscKeyDown);
-  popupLayer.addEventListener('click', onPopupLayerClick);
-};
-
-function onModalCloseButtonClick() {
-  hidePopup();
-  popupLayer.removeEventListener('click', onPopupLayerClick);
-}
-
-if (callbackButton) {
-  callbackButton.addEventListener('click', onCallbackButtonClick);
-}
-
-if (modalCloseButton) {
-  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
-}
-
-var focusLock = window.focusLock;
-
-
-focusLock.on(modalWindow);
-focusLock.off(modalCloseButton);
-
-
-// -------------------accordeon-----------------------------------------
-
-var siteSections = document.querySelector('.page-footer__site-sections');
-var contacts = document.querySelector('.page-footer__our-office');
-
-var siteSectionsContainer = siteSections.querySelector('.page-footer__site-section-container');
-var contactsContainer = contacts.querySelector('.page-footer__contacts');
-
-
-var isSiteSectionsUnrolled = false;
-var isContactsUnrolled = false;
-
-var onSiteSectionsClick = function () {
-  isSiteSectionsUnrolled = !isSiteSectionsUnrolled;
-
-  if (isSiteSectionsUnrolled) {
-    siteSectionsContainer.classList.remove('rolled');
-  } else {
-    siteSectionsContainer.classList.add('rolled');
-  }
-
-  if (isContactsUnrolled) {
-    contactsContainer.classList.add('rolled');
-    isContactsUnrolled = false;
-  }
-};
-
-var onContactsClick = function () {
-  isContactsUnrolled = !isContactsUnrolled;
-
-  if (isContactsUnrolled) {
-    contactsContainer.classList.remove('rolled');
-  } else {
-    contactsContainer.classList.add('rolled');
-  }
-
-  if (isSiteSectionsUnrolled) {
-    siteSectionsContainer.classList.add('rolled');
-    isSiteSectionsUnrolled = false;
-  }
-};
-
-if (siteSections) {
-  siteSections.addEventListener('click', onSiteSectionsClick);
-}
-if (contacts) {
-  contacts.addEventListener('click', onContactsClick);
-}
-
-// -----------------------------------------------------------------
+swiper.init();
