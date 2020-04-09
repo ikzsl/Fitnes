17007f857c5e0fd40fce4edf4e72144727d8cb34
dmm <ikzsl@yandex.ru>
2020-04-09 15:57:38 +0300

    Добавляет слайдер - Отзывы

--- build/js/main.js
+++ build/js/main.js
@@ -19,38 +19,64 @@ var swiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
 
   breakpoints: {
-    // when window width is >= 320px
-
     // when window width is >= 768px
     768: {
       slidesPerView: 2,
       spaceBetween: 2,
       slidesPerGroup: 2,
     },
+    // when window width is >= 1200px
     1200: {
       slidesPerView: 4,
       spaceBetween: 30,
       slidesPerGroup: 4,
     },
-
   },
-  // slidesOffsetBefore: 100,
-  // slidesOffsetAfter: 100,
 
-  // loop: true,
-  // centeredSlides: true,
-
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.button-next1',
+    prevEl: '.button-prev1',
   },
 
 });
 
 swiper.init();
+
+var swiper2 = new Swiper('.swiper-container2', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  // width: 640,
+
+  centeredSlides: true,
+
+  // spaceBetween: 80,
+
+  // Optional parameters
+  direction: 'horizontal',
+  updateOnWindowResize: true,
+  slidesOffsetBefore: 20,
+  // slidesOffsetAfter: 40,
+
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesOffsetBefore: 40,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesOffsetBefore: 40,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next2',
+    prevEl: '.button-prev2',
+  },
+
+});
+
+swiper2.init();
+swiper2.width = 200;
